e8b45333e3d211f8470558e34a9d493cbac08f44
gagayuan <1208230467@qq.com>
2019-04-30 02:28:42 +0800

    fix missing subjects

--- html2pdf.sh
+++ html2pdf.sh
@@ -10,14 +10,16 @@ fi
 var=$1
 page_name=${var##*/}
 page_pr=${page_name%%.*}
-sed -i '/<head>/ r js.txt' ./full_page/$page_pr.html
+sed -i '/<script.*jquery.*<\/script>/d' ./full_page/$page_pr.html
+sed -i '/<head>/ r clean.js' ./full_page/$page_pr.html
+
 
 if [ ! -d pdfs  ];then
   mkdir pdfs
-  google-chrome --headless --disable-gpu --print-to-pdf=./pdfs/$page_pr.pdf ./full_page/$page_pr.html
+  wait_excuted=`google-chrome --headless --disable-gpu --print-to-pdf=./pdfs/$page_pr.pdf ./full_page/$page_pr.html`
   mv ./pdfs/$page_pr.pdf ./pdfs/$2.pdf
 else
-  google-chrome --headless --disable-gpu --print-to-pdf=./pdfs/$page_pr.pdf ./full_page/$page_pr.html
+  wait_excuted=`google-chrome --headless --disable-gpu --print-to-pdf=./pdfs/$page_pr.pdf ./full_page/$page_pr.html`
   mv ./pdfs/$page_pr.pdf ./pdfs/$2.pdf
 fi
 
